6b6062552f306d713df50326b1b07ff8d3911def
cristian <cristian.mitroi@jina.ai>
2020-12-22 09:02:55 +0100

    fix: pip error

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,11 +15,13 @@ echo 'COMPARISON_LEVEL = ', $COMPARISON_LEVEL
 echo 'TEST_AGAIN = ', $TEST_AGAIN
 
 apt-get update -y
-apt-get install python3.7 git python3-distutils python3-pip python3-setuptools -y
+apt-get install python3.7 git python3-distutils -y
 echo `which python3.7`
+curl -sS https://bootstrap.pypa.io/get-pip.py | python3.7
 echo `which pip3`
 echo `pwd`
 echo `ls -la`
+python3.7 -m pip install --upgrade setuptools
 python3.7 -m pip install wheel --user
 python3.7 -m pip install gitpython~=3.1 ruamel.yaml==0.16.12 PyGithub==1.53 semver==2.13.0 --user
 git clone --depth=1 https://$GITHUB_TOKEN@github.com/$MODULES_REPO hub_repo
